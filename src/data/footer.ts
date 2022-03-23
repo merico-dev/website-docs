be1c37e281a633f32c6eb6108110c3d45f4cdf5b
Leto <gerile.tu@merico.dev>
2022-03-23 23:32:46 +0800

    feat: update navbar and footer
    EE-7514

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,77 +1,57 @@
 export const en = [
   {
-    name: 'Open Source Ecosystem',
+    name: '产品',
     items: [
       {
-        name: 'TiDB',
-        url: 'https://pingcap.com/products/tidb',
-      },
-      {
-        name: 'TiKV',
-        url: 'https://github.com/tikv/tikv',
-      },
-      {
-        name: 'TiSpark',
-        url: 'https://github.com/pingcap/tispark',
-      },
-      {
-        name: 'Chaos Mesh',
-        url: 'https://github.com/pingcap/chaos-mesh',
+        name: '官网首页',
+        url: 'https://merico.cn',
       },
     ],
   },
   {
-    name: 'Resources',
+    name: '文档',
     items: [
       {
-        name: 'Quick Start',
-        url: '/tidb/stable/quick-start-with-tidb',
+        name: '快速上手',
+        url: '#',
       },
       {
-        name: 'Community',
-        url: 'https://pingcap.com/community',
+        name: '常见问题解答',
+        url: '#',
+      },
+      {
+        name: '版本发布',
+        url: '#',
       },
     ],
   },
   {
-    name: 'Use Cases',
+    name: '学习资源',
     items: [
       {
-        name: 'Internet',
-        url: 'https://pingcap.com/case-studies/Internet',
+        name: '客户案例',
+        url: '#',
       },
       {
-        name: 'Financial services',
-        url: 'https://pingcap.com/case-studies/Financial-Services',
+        name: '视频课程',
+        url: '#',
       },
       {
-        name: 'Gaming',
-        url: 'https://pingcap.com/case-studies/Gaming',
+        name: '播客',
+        url: '#',
       },
     ],
   },
   {
-    name: 'Company',
+    name: '关于我们',
     items: [
       {
-        name: 'About',
-        url: 'https://pingcap.com/about',
+        name: '了解我们',
+        url: 'https://merico.cn/our-story',
       },
       {
-        name: 'Careers',
-        url: 'https://pingcap.com/careers',
-      },
-      {
-        name: 'Legal',
-        url: 'https://pingcap.com/legal',
-      },
-      {
-        name: 'Blog',
-        url: 'https://pingcap.com/blog',
-      },
-      {
-        name: 'Contact Us',
-        url: 'https://pingcap.com/contact-us',
+        name: '联系我们',
+        url: '#',
       },
     ],
   },
@@ -82,84 +62,55 @@ export const zh = [
     name: '产品',
     items: [
       {
-        name: 'TiDB',
-        url: '/tidb/stable',
+        name: '官网首页',
+        url: 'https://merico.cn',
       },
-      // {
-      //   name: '生态工具',
-      //   url: '/tools',
-      // },
     ],
   },
   {
-    name: '资源',
+    name: '文档',
     items: [
       {
         name: '快速上手',
-        url: '/tidb/stable/quick-start-with-tidb',
-      },
-      {
-        name: '最佳实践',
-        url: '/tidb/stable/tidb-best-practices',
+        url: '#',
       },
       {
         name: '常见问题解答',
-        url: '/tidb/stable/tidb-faq',
+        url: '#',
       },
       {
         name: '版本发布',
-        url: '/tidb/dev/release-notes',
+        url: '#',
       },
     ],
   },
   {
-    name: '学习',
+    name: '学习资源',
     items: [
       {
         name: '客户案例',
-        url: 'https://pingcap.com/zh/case',
+        url: '#',
       },
       {
-        name: 'PingCAP Education',
-        url: 'https://learn.pingcap.com',
+        name: '视频课程',
+        url: '#',
       },
       {
-        name: 'TiDB in Action',
-        url: 'https://book.tidb.io',
+        name: '播客',
+        url: '#',
       },
     ],
   },
   {
-    name: '支持',
+    name: '关于我们',
     items: [
       {
-        name: 'AskTUG',
-        url: 'https://asktug.com',
+        name: '了解我们',
+        url: 'https://merico.cn/our-story',
       },
       {
         name: '联系我们',
-        url: 'mailto:info@pingcap.com',
-      },
-    ],
-  },
-  {
-    name: '公司',
-    items: [
-      {
-        name: '关于我们',
-        url: 'https://pingcap.com/zh/about-us?tab=companyOverview',
-      },
-      {
-        name: '招贤纳士',
-        url: 'https://careers.pingcap.com',
-      },
-      {
-        name: '新闻报道',
-        url: 'https://pingcap.com/zh/about-us?tab=news',
-      },
-      {
-        name: '博客',
-        url: 'https://pingcap.com/zh/blog',
+        url: '#',
       },
     ],
   },
