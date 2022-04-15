9c5e9f3d8fc02f59422427f35ddc751825702ca7
Leto <gerile.tu@merico.dev>
2022-04-15 14:41:18 +0800

    debug: make some changes to downloadPDF link

--- src/components/doc/DownloadPDF.tsx
+++ src/components/doc/DownloadPDF.tsx
@@ -8,11 +8,10 @@ export const DownloadPDF = ({ downloadURL }: Props) => {
   return (
     <a
       className="doc-help-link download-pdf"
-      // href={`/${downloadURL}`}
-      href="https://docs.merico.cn/download/merico_ee_guideline.pdf"
-      target="_blank"
-      rel="noreferrer">
-      <FormattedMessage id="doc.download-pdf" />
+      href="/download/merico_ee_guideline.pdf"
+      target="_blank" rel="noreferrer"
+    >
+      <FormattedMessage id="doc.download-pdf" /> 
     </a>
   )
 }
