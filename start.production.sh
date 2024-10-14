adb49fe871bcdac76d0c0a2fb741091289952605
Dingding Zhang <dingding.zhang@meri.co>
2024-10-14 13:17:14 +0800

    fix: update workflows and scripts

--- start.production.sh
+++ start.production.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 . /root/.nvm/nvm.sh
 nvm use v14.15.0
-yarn build
+# https://github.com/gatsbyjs/gatsby/discussions/32389
+# https://github.com/gatsbyjs/gatsby/discussions/34473
+# https://github.com/pingcap/community-website?tab=readme-ov-file#deploy
+GATSBY_CPU_COUNT=1 yarn build
 mv public/sitemap/sitemap-0.xml public/sitemap.xml
 yarn serve
